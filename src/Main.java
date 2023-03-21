bd2e2f100ebc73299ed5e7e16c0b764011104732
Dima3155 <muxametzyanov.d@mail.ru>
2023-03-21 18:13:34 +0500

    adding min function and average function

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         SalesManager salesManager = new SalesManager(new int[] {200, 2, 4, 20,});
         int max = salesManager.max();
         System.out.println(max);
+
+        int min = salesManager.min();
+        System.out.println(min);
+
+        int average = salesManager.average();
+        System.out.println(average);
     }
 }
